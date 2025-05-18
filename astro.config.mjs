b69a0590747cbf2dff72a018808a86397bb18c9d
Suneet <suneet.bhopal@gmail.com>
2025-05-18 16:28:36 -0400

    git pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import vercel from '@astrojs/vercel';
+// import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
+  // site: "https://positivustheme.vercel.app",
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  // adapter: vercel(),
 });
